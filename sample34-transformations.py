168e7f56c16517b28b5f06ba1703ff697e4f45cc
eu <eugen_hhh@yahoo.com>
2020-01-28 21:04:16 +0200

    -add- reformat transformations exercise

--- sample34-transformations.py
+++ sample34-transformations.py
@@ -1,12 +1,10 @@
-import pathlib
-
-from pyspark.sql import SparkSession
 import pyspark.sql.functions as psf
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import SparkSession
 
 CITIES_CSV = './resources/lesson1/csv/cities.csv'
 LINES_CSV = './resources/lesson1/csv/lines.csv'
 TRACKS_CSV = './resources/lesson1/csv/tracks.csv'
+
 
 def transformation_exercise():
     """
@@ -32,15 +30,12 @@ def transformation_exercise():
 
     # TODO the names of the columns are confusing (two id columns, but they're not matching)
     # TODO how do we solve this problem?
-    #lines_df = lines_df.rdd.map(lambda x: x[0])
     lines_df = lines_df.withColumnRenamed("name", "city_name").select("city_id", "city_name")
     left_df = cities_df.join(lines_df, cities_df.id == lines_df.city_id, "inner")
 
-
     # TODO how do you know which track is still operating?
-    #tracks_df.rdd.filter(lambda x:x[4]==999999).count()
+    # tracks_df.rdd.filter(lambda x:x[4]==999999).count()
     tracks_df.select(psf.max("closure")).distinct().show()
-
 
     # TODO filter on only the operating tracks
     filtered_df = tracks_df.filter("closure = 999999")
@@ -48,11 +43,10 @@ def transformation_exercise():
     joined_df = left_df.join(filtered_df, left_df.city_id == filtered_df.city_id, "inner")
 
     # TODO Q1 and Q2 answers
-    ######
-    #lines_df.flatMap(lambda x:x.lower().split(',')).map(lambda x:(x,1)).reduceByKey(add)
 
     joined_df.select("city_name").distinct().count()
     joined_df.select("city_name").distinct().show(40)
+
 
 if __name__ == "__main__":
     transformation_exercise()
